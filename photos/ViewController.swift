48a4ce7ca9d3f1633670ab10127c0b4a41f4f362
Denis Naughton <mjolinor75@hotmail.com>
2020-10-13 01:19:08 -0400

    first commit

---
+++ photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photos
+//
+//  Created by Admin on 10/13/20.
+//  Copyright © 2020 DASHtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
